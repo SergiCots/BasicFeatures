d7f5eadbad3f037bd26e3529b89fd9a3a59aeda1
SergiCots <sergicots93@gmail.com>
2024-08-07 21:38:22 +0200

    First commit

---
+++ src/main/java/com/sergicots/basicfeatures/BasicFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.sergicots.basicfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicFeaturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicFeaturesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sergicots/basicfeatures/BasicFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergicots.basicfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicFeaturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
